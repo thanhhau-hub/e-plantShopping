150e99c96cc2f77f392466d6d255c888c5eaccda
thanhhau-hub <tothanhhau29012005@gmail.com>
2026-01-15 14:36:58 +0700

    Refactor ProductList component and update plant data

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,50 +1,84 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addItem } from "../redux/CartSlice";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
+import Navbar from "./Navbar";
 
-const plants = {
-  Indoor: [
-    { id: 1, name: "Snake Plant", price: 20, image: "https://via.placeholder.com/150" },
-    { id: 2, name: "Peace Lily", price: 25, image: "https://via.placeholder.com/150" }
-  ],
-  Outdoor: [
-    { id: 3, name: "Rose Plant", price: 15, image: "https://via.placeholder.com/150" },
-    { id: 4, name: "Bamboo Palm", price: 30, image: "https://via.placeholder.com/150" }
-  ]
-};
+const plants = [
+  {
+    id: 1,
+    name: "Aloe Vera",
+    price: 10,
+    image: "/images/aloe.jpg",
+    category: "Indoor",
+  },
+  {
+    id: 2,
+    name: "Snake Plant",
+    price: 12,
+    image: "/images/snake.jpg",
+    category: "Indoor",
+  },
+  {
+    id: 3,
+    name: "Peace Lily",
+    price: 15,
+    image: "/images/lily.jpg",
+    category: "Indoor",
+  },
+  {
+    id: 4,
+    name: "Spider Plant",
+    price: 9,
+    image: "/images/spider.jpg",
+    category: "Indoor",
+  },
+  {
+    id: 5,
+    name: "Rubber Plant",
+    price: 18,
+    image: "/images/rubber.jpg",
+    category: "Indoor",
+  },
+  {
+    id: 6,
+    name: "ZZ Plant",
+    price: 20,
+    image: "/images/zz.jpg",
+    category: "Indoor",
+  },
+];
 
 function ProductList() {
   const dispatch = useDispatch();
-  const [disabledButtons, setDisabledButtons] = useState([]);
+  const cartItems = useSelector((state) => state.cart.items);
 
-  const handleAddToCart = (plant) => {
-    dispatch(addItem({ ...plant, quantity: 1 }));
-    setDisabledButtons([...disabledButtons, plant.id]);
-  };
+  const isAdded = (id) => cartItems.some((item) => item.id === id);
 
   return (
-    <div>
-      <h2>Our Plants</h2>
+    <>
+      <Navbar />
 
-      {Object.keys(plants).map((category) => (
-        <div key={category}>
-          <h3>{category}</h3>
-          {plants[category].map((plant) => (
-            <div key={plant.id}>
+      <div className="product-list">
+        <h2>Our Plants</h2>
+
+        <div className="products">
+          {plants.map((plant) => (
+            <div key={plant.id} className="product-card">
               <img src={plant.image} alt={plant.name} />
-              <h4>{plant.name}</h4>
+              <h3>{plant.name}</h3>
               <p>${plant.price}</p>
+
               <button
-                onClick={() => handleAddToCart(plant)}
-                disabled={disabledButtons.includes(plant.id)}
+                disabled={isAdded(plant.id)}
+                onClick={() => dispatch(addToCart(plant))}
               >
-                Add to Cart
+                {isAdded(plant.id) ? "Added" : "Add to Cart"}
               </button>
             </div>
           ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </>
   );
 }
 
