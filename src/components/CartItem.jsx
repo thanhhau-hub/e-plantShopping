6bca9e73a3c503167523b5fc71659b02b6bc17e8
thanhhau-hub <tothanhhau29012005@gmail.com>
2026-01-15 09:13:54 +0700

    Refactor CartItem component to manage cart state

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,50 @@
-function CartItem({ item }) {
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItem, updateQuantity } from "../redux/CartSlice";
+
+function CartItem() {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.items);
+
+  const totalAmount = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
   return (
     <div>
-      <h3>{item.name}</h3>
-      <p>Quantity: {item.quantity}</p>
+      <h2>Shopping Cart</h2>
 
-      <button>+</button>
-      <button>-</button>
-      <button>Remove</button>
+      {cartItems.map((item) => (
+        <div key={item.id}>
+          <h4>{item.name}</h4>
+          <p>Price: ${item.price}</p>
+          <p>Quantity: {item.quantity}</p>
+
+          <button
+            onClick={() =>
+              dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }))
+            }
+          >
+            +
+          </button>
+
+          <button
+            onClick={() =>
+              dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }))
+            }
+            disabled={item.quantity <= 1}
+          >
+            -
+          </button>
+
+          <button onClick={() => dispatch(removeItem(item.id))}>
+            Remove
+          </button>
+        </div>
+      ))}
+
+      <h3>Total Amount: ${totalAmount}</h3>
     </div>
   );
 }
